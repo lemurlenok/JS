77d501f4673bb4c24259f05f91af4e87284d468f
lemurlenok <lemurlenok@gmail.com>
2024-05-03 18:53:40 +0300

    add hw3-n

--- L-3/hw-3.js
+++ L-3/hw-3.js
@@ -88,22 +88,37 @@ for (let i =0; i <participants .length; i++) {
 // <img src="IMAGE" alt="" class="product-image">
 // </div>
 // Замість TITLE PRICE IMAGE - підставити відповідні поля з об'єкту
+let products = [
+     {
+         title: 'milk',
+         price: 22,
+         image: 'https://www.mcqueensdairies.co.uk/wp-content/uploads/2019/02/Mcqueens_1litre_whole_organic-300x300-3.jpg'
+     },
+     {
+         title: 'juice',
+         price: 27,
+         image: 'https://images-na.ssl-images-amazon.com/images/I/61jL2GCuKLL._SX679_PIbundle-24,TopRight,0,0_AA679SH20_.jpg'
+     },
+     {
+         title: 'tomato',
+         price: 47,
+         image: 'https://dictionary.cambridge.org/ru/images/thumb/tomato_noun_001_17860.jpg?version=5.0.74'
+     },
+     {
+         title: 'tea',
+         price: 15,
+         image: 'https://yogiproducts.com/wp-content/uploads/2009/03/YT-US-CAR-RelaxedMind-C23-202201-V2-3DFront_withGlow-300DPI-1.png'
+     },
+ ];
 
-function writer (productTitle, price, image) {
-    document.write(
-        `
-        <div class="product-card">
-            <h3 class="product-title>${productTitle} ${price}</h3>
-            <img src="${image}" alt="" class="product-image">
-        </div>
-        `
-    )
+for (let i = 0; i < products. length; i++) {
+    const product = products[i]
+    document.write(`<div class="product-card">
+            <h3 class="product-title"> ${product.title} ${product.price}</h3>
+            <img src="${product.image}" alt="" class="product-image">
+        </div>`)
 }
 
-    writer('milk', 22, src="https://www.mcqueensdairies.co.uk/wp-content/uploads/2019/02/Mcqueens_1litre_whole_organic-300x300-3.jpg'", alt=`jpg`);
-    writer('juice', 27, src="https://images-na.ssl-images-amazon.com/images/I/61jL2GCuKLL._SX679_PIbundle-24,TopRight,0,0_AA679SH20_.jpg'");
-    writer('tomato', 47, src="'https://dictionary.cambridge.org/ru/images/thumb/tomato_noun_001_17860.jpg?version=5.0.74'");
-    writer('tea', 15, src="'https://yogiproducts.com/wp-content/uploads/2009/03/YT-US-CAR-RelaxedMind-C23-202201-V2-3DFront_withGlow-300DPI-1.png'")
 
 // є масив
 // let users = [
@@ -138,9 +153,7 @@ let users = [
      {name: 'max', age: 31, status: true}
 ]
 
-for (const user of users) {
-    for (const filedName in user) {
-        console.log(user['status'])
+for (let i = 0; i<users.length ; i++) {
+    document.write(`<h2> ${users [i].status}</h2>`)
     }
     
-}
