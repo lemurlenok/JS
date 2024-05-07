20056a43f4e3cfbb4b6ac7e42a544fc0fc4d2bcd
lemurlenok <lemurlenok@gmail.com>
2024-05-07 11:54:49 +0300

    add hw3-n3

--- L-3/hw-3.js
+++ L-3/hw-3.js
@@ -1,5 +1,5 @@
 
-//- За допомогою циклу for і document.write() вивести 10 блоків div c довільним текстом всередині
+    //- За допомогою циклу for і document.write() вивести 10 блоків div c довільним текстом всередині
 
 let participants  = [
     {name: 'Sonya',  username: '@Sonya', age: 18},
@@ -13,43 +13,78 @@ let participants  = [
     {name: 'Danya', username: '@Danya', age: 19},
     {name: 'Orestes', username: '@Orestes', age: 30}
 ]
-for (let i =0; i <participants .length; i++) {
-    const participant = participants [i];
+for (const participant of participants) {
     document.write(`<div>
                         <h2>${participant.name} - ${participant.username} - ${participant.age}</h2>
                    </div>`
     )
 }
+
+// for (let i =0; i <participants .length; i++) {
+//     const participant = participants [i];
+//     document.write(`<div>
+//                         <h2>${participant.name}:</h2>
+//                         <li>${participant.username} - ${participant.age}</li>
+//
+//                    </div>`
+//     )
+// }
 //- За допомогою циклу for і document.write() вивести 10 блоків div c довільним текстом і індексом всередині
 
+    for (let i = 0; i < 10; i++) {
+        document.write('<div class="block">Цей блок ' + i + '</div>');
+    }
+
+// for (let i = 0; i < 10; i++) {
+//     document.write(`<div class="box">
+//                             <div class="box2">Зовнішність Crazy Rabbit може змінюватися в залежності від його настрою: від безтурботної посмішки до дивакуватих виразів обличчя, завжди підкресленої його незбагненною енергією. ${i+1}</div>
+// </div>`);
+// }
 //- За допомогою циклу while вивести в документ 20 блоків h1 c довільним текстом всередині.
 
+let count = 0;
+while (count < 20) {
+    document.write(`<div class="bees"> :
+ <h1>бджоли ділять на:
+    <li> **Матки**: Матки відповідають за вирощування нових бджіл та управління вуликом. Вони відкладають яйця, які вилуплюються в робочих бджіл та дронів. Матка також вирішує, коли створювати нову матку або розпочинати новий рой.</li>
+    <li>**Робочі бджоли**: Робочі бджоли складають більшість популяції вулика. Вони займаються збиранням нектару та пилку, будівництвом та утриманням вулика, годуванням личинок та маток, а також обороняють вулик від нападів.</li> 
+    <li>**Трутні**: Дрони - це чоловічі бджоли, їх головна функція - розмноження. Вони здатні оплодотворювати маток під час полювання на нові колонії. Дрони не беруть участь у збиранні нектару чи пилку або будівництві вулика, тому їх кількість в колонії може змінюватися в залежності від потреб колонії та сезону.</li>
+ </h1>
+ </div>`);
+    count++;
+
+}
 
 //- За допомогою циклу while вивести в документ 20 блоків h1 c довільним текстом і індексом всередині.
-
+let line = 0;
+while (line < 20) {
+    document.write(`<h1> ${line + 1} 
+                        <li> Кожна бджола віддана своєму завданню та знає, що її праця має велике значення для спільної мети - забезпечення виживання колонії.</li>
+                
+    </h1>`);
+    line++;
+}
 
 //Використовуючи данні з масиву, за допомоги document.write та циклу побудувати структуру по шаблону
 //Масив:
-//    let listOfItems = ['html', 'css', 'javascript', 'mysql', 'mongodb', 'react', 'angular', 'node.js'];
+   let listOfItems = ['html', 'css', 'javascript', 'mysql', 'mongodb', 'react', 'angular', 'node.js'];
 //ШАБЛОН:
-// function write(){
-//     document.write(
-//         <ul>
-//             <li>'html'</li>
-//             <ul>
-//                 <li>'css'</li>
-//                 <li>'javascript'</li>
-//                 <li>'mysql'</li>
-//                 <li>'mongodb'</li>
-//                 <li>'react'</li>
-//                 <li>'angular'</li>
-//                 <li>'node.js'</li>
-//             </ul>
-//         </ul>
-//     )
-// }
-// write();
+//ШАБЛОН:
+//  <ul>
+//     <li>ITEM OF ARRAY</li>
+//     <!--
+//         і тд інші об'єкти масиву
+//          ...
+//          ...
+//          ...
+//     -->
+// </ul>
 
+document.write('<ul>');
+for (let i = 0; i < listOfItems.length; i++) {
+    document.write(`<li>'${listOfItems[i]}'</li>`);
+}
+document.write('</ul>');
 
 //замість 'ITEM OF ARRAY' підставити елемент з масиву щоб получився цілий список з даними з масиву
 
@@ -155,31 +190,32 @@ let users = [
      {name: 'olya', age: 31, status: false},
      {name: 'max', age: 31, status: true}
 ]
-
+// Користувачі зі статусом true
 for (let i = 0; i<users.length ; i++) {
     if (users [i].status === false){
         continue;
     }
-    document.write(`<h2> ${users[i].name}, ${users [i].status}</h2>`)
+    console.log(users[i].name, users[i].status)
+    document.write(`<h2> ${users[i].name}: ${users [i].status}</h2>`)
 }
+// Користувачі зі статусом false
 for (let i = 0; i<users.length ; i++) {
     if (users [i].status === true){
         continue;
     }
-    document.write(`<h2> ${users [i].status}, ${users[i].name}</h2>`)
+    console.log(users[i].status, users[i].name)
+    document.write(`<ul class="dw"> ${users [i].status}, ${users[i].name}</ul>`)
 }
+// Користувачі старші за 30 років
 for (let i = 0; i<users.length ; i++) {
     let user = users[i];
     if (users [i].age <31){
         continue;
     }
     console.log(user[`name`], user[`age`]);
+    document.write(`<div>
+                           <h2>${users[i].name}, ${users[i].age} років</h2>
+
+</div>`)
 }
 
-    // let i =0
-    // while (i<users.length){
-    //     if (users [i].status === true){
-    //         console.log(users[i]);
-    //         i++;
-    //     }
-    // }
