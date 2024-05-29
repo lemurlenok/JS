35b76ac52e3a7e5b2f1db7bf77a7a45e4ffeb8f6
lemurlenok <lemurlenok@gmail.com>
2024-05-29 18:08:44 +0300

    add hw-11

--- L-11/hw-11.js
+++ L-11/hw-11.js
@@ -44,45 +44,51 @@
                 recipeName.innerText = recipe.name;
                 divRecipesContainer.appendChild(recipeName);
 
+                let divInstruction = document.createElement('div');
+                divInstruction.classList = 'instruction';
+
+                let recipeID = document.createElement('p');
+                recipeID.innerHTML = `<strong>ID:</strong> ${recipe.id}`;
+                divRecipesContainer.appendChild(recipeID);
+
                 let divImage = document.createElement('div');
                 let recipeImage = document.createElement('img');
                 recipeImage.src = recipe.image;
                 divImage.appendChild(recipeImage);
                 divRecipesContainer.appendChild(divImage);
 
+                let ul = document.createElement('ul');
+                ul.innerHTML = `<h3>Ingredients:</h3>`;
+                for (const ingredient of recipe.ingredients) {
+                    let li = document.createElement('li');
+                    li.innerText = `${ingredient}`;
+                    ul.append(li);
+                }
+
+                let ol = document.createElement('ol');
+                ol.innerHTML = `<h3> Instruction:</h3>`;
+                for (const instruct of recipe.instructions) {
+                    let li = document.createElement('li');
+                    li.innerText = `${instruct}`;
+                    ol.append(li);
+                }
+
+                divInstruction.append(ul, ol);
+                divRecipesContainer.appendChild(divInstruction);
+
                 let recipeDescription = document.createElement('div');
                 recipeDescription.innerHTML = `
-                <p><strong>ID:</strong>  ${recipe.id} </p>
-                <p><strong>Prep Time: </strong>${recipe.prepTimeMinutes} minutes</p>
-                <p><strong>Cook Time: </strong>${recipe.cookTimeMinutes} minutes</p>
-                <p><strong>Servings: </strong>${recipe.servings} portions</p>
-                <p><strong>Difficulty: </strong>${recipe.difficulty}</p>
-                <p><strong>Cuisine: </strong>${recipe.cuisine}</p>
-                <p><strong>Calories Per Serving: </strong>${recipe.caloriesPerServing} ckal</p>
-                <p><strong>Tags: </strong>${recipe.tags}</p>`;
+                    <p><strong>Prep Time: </strong>${recipe.prepTimeMinutes} minutes</p>
+                    <p><strong>Cook Time: </strong>${recipe.cookTimeMinutes} minutes</p>
+                    <p><strong>Servings: </strong>${recipe.servings} portions</p>
+                    <p><strong>Difficulty: </strong>${recipe.difficulty}</p>
+                    <p><strong>Cuisine: </strong>${recipe.cuisine}</p>
+                    <p><strong>Calories Per Serving: </strong>${recipe.caloriesPerServing} ckal</p>
+                    <p><strong>Tags: </strong>${recipe.tags}</p>`;
                 divRecipesContainer.appendChild(recipeDescription);
 
-                let divInstruction = document.createElement('div')
-                divInstruction.classList = 'instruction'
 
-                let ul = document.createElement('ul')
-                ul.innerHTML = `<h3>Ingredients:</h3>`
-                for (const ingredient of recipe.ingredients) {
-                    let li = document.createElement('li')
-                    li.innerText = `${ingredient}`
-                    ul.append(li)
-                }
 
-                let ol = document.createElement('ol')
-                ol.innerHTML = `<h3> Instruction:</h3>`
-                for (const instruct of recipe.instructions) {
-                    let li = document.createElement('li')
-                    li.innerText = `${instruct}`
-                    ol.append(li)
-                }
-
-                divInstruction.append(ul, ol)
-                divRecipesContainer.appendChild(divInstruction);
 
 
                 recipesBlock.appendChild(divRecipesContainer);
